024414631a4fd93ae1a2774b0bf0135b13ff0898
shalin-jain <sjain441@gatech.edu>
2023-02-04 15:16:30 -0500

    detection testing

--- landmarks.py
+++ landmarks.py
@@ -2,35 +2,56 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import processing
+import os
 from time import sleep
 
 mp_hands = mp.solutions.hands
 
 hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5)
-image = cv2.flip(cv2.imread("image.jpg"), 1)
+test = cv2.flip(cv2.imread("pos.jpg"), 1)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
+images = []
+for file in os.listdir("./images"):
+    distorted = processing.distort(cv2.imread("./images/" + file))
+    for image in distorted:
+        landmarks = processing.get_landmarks(image)
+        if landmarks is not None:
+            images.append(processing.get_distances(landmarks))
+
+images = np.array(images)
+mean_std= processing.mean_std(images)
+
 def process(image):
     image.flags.writeable = False
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
- 
     landmarks = processing.get_landmarks(image)
-    if landmarks is not None:
-        print(processing.get_distances(landmarks))
-
-    if results.multi_hand_landmarks:
-          for hand_landmarks in results.multi_hand_landmarks:
+    if results.multi_hand_landmarks and landmarks is not None:
+        for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS,
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
+
+        distances = processing.get_distances(landmarks)
+        print(distances)
+        diff = np.abs(np.subtract(mean_std[:,0], distances))
+        print(np.count_nonzero(np.less_equal(diff, mean_std[:, 1])) / 21)
+        if np.less_equal(diff, mean_std[:, 1] * 2).all():
+            print("fuck you")
+        else:
+            print("damn")
+
     return image
 
+process(test)
 
+
+"""
 vid = cv2.VideoCapture(0)
 
 while vid.isOpened():
@@ -39,7 +60,7 @@ while vid.isOpened():
         image = process(image)
     cv2.imshow('', image)
 
-    sleep(0.05)
+    sleep(0.01)
 
     # PREVIEW
     if cv2.waitKey(1) & 0xFF==ord('q'):
@@ -47,3 +68,4 @@ while vid.isOpened():
 
 vid.release()
 cv2.destroyAllWindows()
+"""
