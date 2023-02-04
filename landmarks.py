094cbc6dcd934854d3e16c80e96e1fb49560710f
epicmonky <akash20952@gmail.com>
2023-02-04 14:11:29 -0500

    funny landmarks (will crash your computer)

--- landmarks.py
+++ landmarks.py
@@ -2,40 +2,64 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import processing
+from collections import deque
+from multiprocessing.pool import ThreadPool
 
 mp_hands = mp.solutions.hands
 
 hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5)
 image = cv2.flip(cv2.imread("image.jpg"), 1)
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
-# Convert the BGR image to RGB before processing.
-results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-array = []
+def process(image):
+    image.flags.writeable = False
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+ 
+    landmarks = processing.get_landmarks(image)
+    if landmarks is not None:
+        print(processing.get_distances(landmarks))
 
-for landmark in results.multi_hand_landmarks:
-    for i in range(21):
-        array.append([landmark.landmark[i].x, landmark.landmark[i].y, landmark.landmark[i].z])
+    if results.multi_hand_landmarks:
+          for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+    return image
 
-array = np.array(array)
 
-z_array = array[:,2]
+vid = cv2.VideoCapture(2)
 
-array = array[:,[0, 1]]
+thread_num = cv2.getNumberOfCPUs()
+pool = ThreadPool(processes=thread_num)
+pending = deque()
 
-z_array = np.tile(z_array, (2, 1)).transpose() + 1
+while vid.isOpened():
 
-normal_array = np.divide(array, z_array)
+    # CONSUME
+    while len(pending) > 0 and pending[0].ready():
+        res = pending.popleft().get()
+        cv2.imshow('', res)
 
-print(normal_array)
+    # POPULATE
+    if len(pending) < thread_num:
+        ret, image = vid.read()
+        if ret:
+            task = pool.apply_async(process, (image.copy(),))
+            pending.append(task)
 
-wrist = np.tile(normal_array[0], (21, 1))
+    # PREVIEW
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
 
-distances = np.sum(np.square(np.subtract(normal_array, wrist)), axis=1)
-print(distances)
 
-dst = processing.distort(image)
-for img in dst:
-    cv2.imshow('', img)
-    cv2.waitKey(0)
+    cv2.imshow('Hands', cv2.flip(image, 1))
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
 
+vid.release()
+cv2.destroyAllWindows()
