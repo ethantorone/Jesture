c61c503c5ede90b7bda9aec1a992c81b0f8a1337
virajsingh7789 <viraj.singh7789@gmail.com>
2023-02-04 18:12:18 -0500

    added action options and auto scroll on image submit

--- jesture-frontend/src/Components/gestureCreate.js
+++ jesture-frontend/src/Components/gestureCreate.js
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import gestureCreateStyles from "./gestureCreate.modules.css";
 
 const WebcamCapture = () => {
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
-  const [displayGestureSelect, setDisplayGestureSelect] = useState(false);
+  const [displayActionSelect, setDisplayActionSelect] = useState(false);
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
   }, [webcamRef, setImgSrc]);
+
+  const onSubmitImage = () => {
+    setDisplayActionSelect(true);
+  };
+
+  useEffect(() => {
+    const element = document.getElementById("pageBottom");
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  }, [displayActionSelect]);
 
   return (
     <div className="cameraAndButtons">
@@ -29,43 +40,45 @@ const WebcamCapture = () => {
       {imgSrc ? 
         <div className="buttons">
           <button className="captureButton" onClick={() => setImgSrc(null)}>TRY AGAIN</button>
-          <button className="captureButton">SUBMIT GESTURE</button>
+          <button className="captureButton" onClick={onSubmitImage}>SUBMIT GESTURE</button>
         </div>
       :
         <button className="captureButton" onClick={capture}>CAPTURE PHOTO</button>
       }
+
+      {displayActionSelect ? 
+        <ActionSelect/> 
+        : 
+        <></>
+      }
+      <div className="bottomOfPage" id="pageBottom"></div>
     </div>
   );
 };
 
 const ActionSelect = () => {
   return (
-    <div>
+    <div className="actionSelection">
       <h1>SELECT AN ACTION TO ASSOCIATE WITH YOUR GESTURE :</h1>
       <form className="actionsList">
         <label>
-          <input type="radio" value="option1" checked={true} />
-          PAUSE/PLAY MUSIC
+          <input type="radio" value="pauseAndPlay" name="actions"/>
+          <label>PAUSE/PLAY MUSIC</label>
         </label>
         <label>
-          <input type="radio" value="option1" checked={true} />
-          INCREASE VOLUME
+          <input type="radio" value="incVol" name="actions" />
+          <label>INCREASE VOLUME</label>
         </label>
         <label>
-          <input type="radio" value="option1" checked={true} />
-          DECREASE VOLUME
+          <input type="radio" value="decVol" name="actions"/>
+          <label>DECREASE VOLUME</label>
         </label>
         <label>
-          <input type="radio" value="option1" checked={true} />
-          PLAY SONG
-        </label>
-        <label>
-          <input type="radio" value="option1" checked={true} />
-          OTHER
+          <input type="radio" value="playSong" name="actions"/>
+          <label>PLAY SONG:  <input type='text' className="songTitleBox"></input> </label>
         </label>
       </form>
     </div>
-
   );
 };
 
@@ -73,7 +86,6 @@ function GestureCreate(props) {
   return (
     <div>
       <WebcamCapture/>
-      <ActionSelect/>
     </div>
   );
 };
