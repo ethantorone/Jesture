c2303b296235469aecab3a2a2ec14716a2d20800
virajsingh7789 <viraj.singh7789@gmail.com>
2023-02-04 20:41:18 -0500

    gets value from action options

--- jesture-frontend/src/Components/gestureCreate.js
+++ jesture-frontend/src/Components/gestureCreate.js
@@ -57,26 +57,40 @@ const WebcamCapture = () => {
 };
 
 const ActionSelect = () => {
+  const [selectedAction, setSelectedAction] = useState("");
+
+  function onValueChange(event){
+    setSelectedAction(event.target.value);
+  }
+
+  function createGesture(event) {
+    event.preventDefault();
+    console.log(selectedAction);
+  }
+
+  
+
   return (
     <div className="actionSelection">
       <h1>SELECT AN ACTION TO ASSOCIATE WITH YOUR GESTURE :</h1>
       <form className="actionsList">
         <label>
-          <input type="radio" value="pauseAndPlay" name="actions"/>
+          <input type="radio" value="pauseAndPlay" onChange={onValueChange} name="actions"/>
           <label>PAUSE/PLAY MUSIC</label>
         </label>
         <label>
-          <input type="radio" value="incVol" name="actions" />
+          <input type="radio" value="incVol" onChange={onValueChange} name="actions" />
           <label>INCREASE VOLUME</label>
         </label>
         <label>
-          <input type="radio" value="decVol" name="actions"/>
+          <input type="radio" value="decVol" onChange={onValueChange} name="actions"/>
           <label>DECREASE VOLUME</label>
         </label>
         <label>
-          <input type="radio" value="playSong" name="actions"/>
+          <input type="radio" value="playSong" onChange={onValueChange} name="actions"/>
           <label>PLAY SONG:  <input type='text' className="songTitleBox"></input> </label>
         </label>
+        <button className="createGestureButton" onClick={createGesture}>CREATE GESTURE</button>
       </form>
     </div>
   );
